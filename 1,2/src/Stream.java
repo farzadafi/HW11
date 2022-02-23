54491c2c6b8a1cbd9ffd5acb9c824288ee270a8d
farzad <farzadafi40@gmail.com>
2022-02-23 13:25:44 +0330

    add answer for question Two

--- 1,2/src/Stream.java
+++ 1,2/src/Stream.java
@@ -5,8 +5,18 @@ import java.util.stream.Collectors;
 public class Stream {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("aa","ab","cat","dog","hash","angel","glass","frame","farzad","afshar");
-        list.stream().collect(Collectors.groupingBy(String::length)).forEach((k, v) -> System.out.println(String.format("{%d=%s}",k,v)));
+        System.out.println("Question 1:");
+        list.stream()
+                .collect(Collectors
+                .groupingBy(String::length))
+                .forEach((k, v) -> System.out.println(String.format("{%d=%s}",k,v)));
         //System.out.println(list.stream().collect(Collectors.groupingBy(String::length)));
+
+        System.out.println("\nQuestion 2:");
+        list.stream()
+                .collect(Collectors.groupingBy(String::length))
+                .forEach((k,v) -> System.out.printf("{%d=[%d]}%n",k ,v.size()));
+
 
     }
 
