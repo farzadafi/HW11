212af43b5bc9759aacfdc1cc5ece1d19b8cd6427
farzad <farzadafi40@gmail.com>
2022-02-23 17:07:48 +0330

    add all field and constructor and getter & setter and toString method

--- 3/src/Product.java
+++ 3/src/Product.java
@@ -1,2 +1,44 @@
 public class Product {
+    private Category category;
+    private String name;
+    private int price;
+
+    public Product(Category category, String name, int price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "category=" + category +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
