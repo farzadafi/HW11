ae0560e8d16cb03f036c1810b6bee8a4cf03ba34
farzad <farzadafi40@gmail.com>
2022-02-23 13:21:30 +0330

    add Stream for exercise one and two

---
+++ 1,2/src/Stream.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        
+
+    }
+
+
+
+
+
+
+}
