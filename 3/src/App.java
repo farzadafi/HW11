62794ada64682f8873941b6a0fcef81dc5819f57
farzad <farzadafi40@gmail.com>
2022-02-23 17:11:29 +0330

    add four Order to a orderList

--- 3/src/App.java
+++ 3/src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class App {
@@ -20,6 +21,14 @@ public class App {
                 .filter(product -> product.getCategory().equals(Pc))
                 .forEach(product -> System.out.println(product.toString()));
          */
+
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(new Order(new Customer("Farzad"),new Product(mobile,"samsung",20000),new Date(2012,1,10)));
+        orderList.add(new Order(new Customer("Farzad"),new Product(mobile,"samsung",20000),new Date(2012,1,10)));
+        orderList.add(new Order(new Customer("Amin"),new Product(Tv,"LG",20000),new Date(2014,10,15)));
+        orderList.add(new Order(new Customer("Reza"),new Product(Pc,"Asus",20000),new Date(2021,9,10)));
+        Date dateAfter  = new Date(2010,10,10);
+        Date dateBefore = new Date(2020,10,10);
 
 
     }
