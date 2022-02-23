842ea85a02c59630a24eba31b78f10aa4b87bade
farzad <farzadafi40@gmail.com>
2022-02-23 17:08:54 +0330

    add three new category

--- 3/src/App.java
+++ 3/src/App.java
@@ -1,2 +1,10 @@
 public class App {
+    public static void main(String[] args) {
+        Category mobile = new Category("mobile");
+        Category Tv = new Category("TV");
+        Category Pc = new Category("PC");
+
+
+
+    }
 }
