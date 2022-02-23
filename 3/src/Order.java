a474612ed41f2924688fe96e24819841e8d349e2
farzad <farzadafi40@gmail.com>
2022-02-23 17:07:45 +0330

    add all field and constructor and getter & setter and toString method

--- 3/src/Order.java
+++ 3/src/Order.java
@@ -1,2 +1,47 @@
+import java.util.Date;
+
 public class Order {
+    private Customer customer;
+    private Product product;
+    private Date date;
+
+    public Order(Customer customer, Product product, Date date) {
+        this.customer = customer;
+        this.product = product;
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "customer=" + customer +
+                ", product=" + product +
+                ", date=" + date +
+                '}';
+    }
 }
+
